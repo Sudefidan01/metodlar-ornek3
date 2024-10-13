784583bd7cf53a6ce6144d26bed6861948bb8852
sude <sudefidan@icloud.com>
2024-10-13 18:33:18 +0300

    metodlar-ornek3

---
+++ 20230812_Metodlar_Ornek3/20230812_Metodlar_Ornek3/Metod.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20230812_Metodlar_Ornek3
+{
+    internal class Metod
+    {
+        public string StringIste(string baslik)
+        {
+            string txt=String.Empty;
+            bool hata = true;
+            do
+            {
+                Console.WriteLine(baslik);
+                txt= Console.ReadLine();
+                if (txt.Length==0)
+                {
+                    hata = true;
+                }
+                else
+                {
+                    hata = false;
+                }
+                
+            } while (hata);
+            return txt;
+        }
+    }
+}
---
+++ 20230812_Metodlar_Ornek3/20230812_Metodlar_Ornek3/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20230812_Metodlar_Ornek3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Kullanıcıdan bir string değer talep edelim
+            //Kullanıcı boş bir değer girdiği sürece odeğeri kullanıcıdan tekrar talep edelim
+            //Kullanıcıdan değer talep etme işlemlerini metod ile yapalım
+            Metod m=new Metod();
+
+            string deger = m.StringIste("Bir Değer Giriniz ");
+
+            Console.WriteLine("Girilen Değer = " + deger);
+
+            //Console.WriteLine(m.StringIste("Bir Değer Girin"));
+
+            Console.ReadKey();
+
+        }
+    }
+}
